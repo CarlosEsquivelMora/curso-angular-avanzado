8b87ddd5cda48cd975356e48d6c24d217006db83
Carlos Daniel Esquivel Mora <carlos.esquivel@vianney.mx>
2025-08-21 09:09:46 -0600

    Primer Commit

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,27 +1,49 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
-import { HeaderComponent } from '@shared/components/header/header.component';
+
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
+import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
 
 @Component({
-    selector: 'app-about',
-    imports: [CommonModule, CounterComponent, WaveAudioComponent, HighlightDirective, HeaderComponent],
-    templateUrl: './about.component.html'
+  selector: 'app-about',
+  imports: [
+    CommonModule,
+    CounterComponent,
+    WaveAudioComponent,
+    HighlightDirective,
+    FormsModule,
+  ],
+  templateUrl: './about.component.html',
 })
 export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
+  osbWithInit$ = new BehaviorSubject<string>('init value');
+  $withInit = toSignal(this.osbWithInit$, { requireSync: true });
+
+  osbWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.osbWithoutInit$.pipe(delay(3000)), {
+    initialValue: '----',
+  });
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
-    this.duration.set(input.valueAsNumber) 
+    this.duration.set(input.valueAsNumber);
   }
 
-  changeMessage(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.message.set(input.value) 
+  changeMessage(event: string) {
+    console.log('changeMessage', event);
+  }
+
+  emitWithInit() {
+    this.osbWithInit$.next('new value');
+  }
+  emitWithoutInit() {
+    this.osbWithoutInit$.next('*****');
   }
 }
